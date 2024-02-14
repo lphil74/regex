7992757513ab24b0edea91f7f87d8e6e68e1552c
lphil74 <lphil74@yahoo.fr>
2024-02-14 16:37:49 +0100

    commit index et menu

---
+++ menuprincipal.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Accueil REGEX</title>
+  </head>
+
+  <body>
+  <p><a href="index.php">Retour à l'index </a></p>
+  <p><a href=index.php?action=regex>TPExpression</a></p>
+</body>
+</html>
